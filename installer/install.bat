f295cf2d1133f07c32cfa431c5fe2cdb6326f0a3
mohamed <mohamed_moanis@outlook.com>
2015-07-16 13:36:54 +0200

    modified the installer to check for mingw installation

--- installer/install.bat
+++ installer/install.bat
@@ -1,9 +1,17 @@
-IF EXIST C:\MinGW\bin\gcc.exe IF EXIST C:\MinGW\bin\g++.exe (
-	IF EXIST srm.exe IF EXIST WinC.dll IF EXIST SharpShell.dll (
+:: WinC installer
+@echo off
+
+:: try to run gcc and g++
+:: if MinGW is defined in path, this should work, other wise it will fail
+gcc.exe > NUL 2> NUL
+g++.exe > NUL 2> NUL
+
+if not %ERRORLEVEL%==9009 (
+	IF EXIST srm.exe IF EXIST WinC.dll IF EXIST SharpShell.dll IF EXIST config.xml (
 		srm install WinC.dll -codebase
 	) ELSE (
 		ECHO "Missing installation files"
 	)
 ) ELSE (
-	ECHO "Please install MinGW compiler!"
+	ECHO "Please install and add MinGW to PATH!"
 )
